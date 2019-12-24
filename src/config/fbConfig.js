768cfc81584cfe8c8e7a4e064caa5828dc71fff1
PLURALITY <githubazulu19@gmail.com>
2019-12-24 15:59:18 +0200

    matcha

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -4,13 +4,7 @@ import 'firebase/auth';
 import 'firebase/storage';
 
 var firebaseConfig = {
-    apiKey: "AIzaSyB8sKJ4ksbcKBxumkDd1EHqHEcEI8W-9iA",
-    authDomain: "matcha-e48de.firebaseapp.com",
-    databaseURL: "https://matcha-e48de.firebaseio.com",
-    projectId: "matcha-e48de",
-    storageBucket: "matcha-e48de.appspot.com",
-    messagingSenderId: "836600186003",
-    appId: "1:836600186003:web:952bcf98c35a35df"
+    apiKey: "*********************************",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
